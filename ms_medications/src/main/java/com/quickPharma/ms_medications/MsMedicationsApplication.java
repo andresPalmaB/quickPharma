acad881587a6d5768502059439e1f3de76c8d8b3
andresPalmaB <palmabustosandres@gmail.com>
2024-11-21 19:05:19 -0500

    Organized microservices of the project

---
+++ ms_medications/src/main/java/com/quickPharma/ms_medications/MsMedicationsApplication.java
@@ -0,0 +1,13 @@
+package com.quickPharma.ms_medications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMedicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMedicationsApplication.class, args);
+	}
+
+}
---
+++ ms_medications/src/test/java/com/quickPharma/ms_medications/MsMedicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickPharma.ms_medications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMedicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
